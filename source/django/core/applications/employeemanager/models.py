de59398a869f36ae283b757e3fecf96ef6ea788b
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-07 10:29:44 -0400

    feat(employeemanager): create Employee model and shift logic to backend
    - Add Employee model with unique email and user foreign key
    - Create initial migration
    - Protect view with login_required
    - Start handling POST requests in view (debug logs)
    - Remove client-side fetch logic from template

--- source/django/core/applications/employeemanager/models.py
+++ source/django/core/applications/employeemanager/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Employee(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    submitted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
--- source/django/core/applications/employeemanager/views.py
+++ source/django/core/applications/employeemanager/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from applications.employeemanager.server.handlers.employeeHandler import employeeHandler
+from django.contrib.auth.decorators import login_required
+from .models import Employee
 
+
+@login_required
 def employeemanager(request):
+    if request.method == "POST":
+            # Employee.objects.create(
+            #     firstname=request.POST.get("fname"),
+            #     lastname=request.POST.get("lname"),
+            #     role=request.POST.get("role"),
+            #     email=request.POST.get("email"),
+            #     submitted_by=request.user             
+            # )
+
+            print(f"signed user: {request.user}")
+            print(f"first name: {request.POST.get("fname")}")
+            print(f"last name: {request.POST.get("lname")}")
+            print(f"role: {request.POST.get("role")}")
+            print(f"email: {request.POST.get("email")}")
+
+            # employees = Employee.objects.filter(submitted_by=request.user)
+
+
     return render(request, "employeemanager/employeemanager.html", {
         "title": "AvantKeel"
     })
